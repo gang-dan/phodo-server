993b5976c69533f29547151d2884e02eb3691787
daeunkwak <misodaeun3@naver.com>
2023-05-10 21:59:41 +0900

    Initial commit

---
+++ phodo-api/src/main/java/app/gangdan/phodoapi/PhodoApiApplication.java
@@ -0,0 +1,13 @@
+package app.gangdan.phodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhodoApiApplication.class, args);
+    }
+
+}
---
+++ phodo-api/src/test/java/app/gangdan/phodoapi/PhodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gangdan.phodoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhodoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
